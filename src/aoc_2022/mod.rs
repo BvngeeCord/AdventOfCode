3609f443753f814ff5b66fb732c4fdd7e5b5e8e1
BvngeeCord <nystromjp@gmail.com>
2022-12-07 00:58:11 -0800

    day 3

--- src/aoc_2022/mod.rs
+++ src/aoc_2022/mod.rs
@@ -2,10 +2,12 @@ use crate::Solver;
 
 pub mod day_1;
 pub mod day_2;
+pub mod day_3;
 
 pub fn solvers_2022() -> Vec<Box<dyn Solver>> {
     vec![
         Box::from(day_1::Day1{}),
-        Box::from(day_2::Day2{})
+        Box::from(day_2::Day2{}),
+        Box::from(day_3::Day3{}),
     ]
 }
